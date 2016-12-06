80cb006a82f895601e26c43cd59ea7204ae56e6d
crodriguezpy <carlos@galaxy-2.local>
2016-12-06 17:02:25 +1100

    + Fixed visualization issues in mainfeed.html and review.html + Added the api/ directory and the YAML (swagger) file for the APIs

--- js/mainfeed.js
+++ js/mainfeed.js
@@ -1,27 +1,43 @@
 $( "#postbtn" ).click(function() {
     removeClasses();
+        
+    $("#posttxt").attr("placeholder", "write you post here...");
     
-    $( "#postbtn" ).addClass("btn btn-info");
-    $( "#questionbtn" ).addClass("btn btn-secondary");
-    $( "#reviewbtn" ).addClass("btn btn-secondary");    
+    $( "#postbtn" ).addClass("btn btn-info btn-sm");
+    $( "#questionbtn" ).addClass("btn btn-secondary btn-sm");
+    $( "#reviewbtn" ).addClass("btn btn-secondary btn-sm");    
     
     $( "#postoptions" ).empty();
+    
+    
+    $("#targetusers").removeAttr("disabled"); 
+    
 });
 
 $( "#questionbtn" ).click(function() {
     removeClasses();
     
-    $( "#postbtn" ).addClass("btn btn-secondary");
-    $( "#questionbtn" ).addClass("btn btn-success");
-    $( "#reviewbtn" ).addClass("btn btn-secondary");  
+    $("#posttxt").attr("placeholder", "ask your question here...");
+    
+    $("#targetusers").val("");
+    
+    $("#targetusers").attr("disabled", "disabled");
+        
+    
+    $( "#postbtn" ).addClass("btn btn-secondary btn-sm");
+    $( "#questionbtn" ).addClass("btn btn-success btn-sm");
+    $( "#reviewbtn" ).addClass("btn btn-secondary btn-sm");  
     
     $( "#postoptions" ).empty();
     
     $( "#postoptions" ).append(
       `
-        <div class="checkbox">
-          <label><input id="chkanonym" type="checkbox" value="">Ask anonymously</label>
+        <div class="col-md-7">
+          <input id="chkchecked"  type="checkbox" value=""> I checked similar questions/answers</input>
         </div>
+        <div class="col-md-5">
+          <input id="chkanonym"  type="checkbox" value=""> Ask anonymously</input>  
+        </div>        
       `
     );
 });
@@ -29,16 +45,20 @@ $( "#questionbtn" ).click(function() {
 $( "#reviewbtn" ).click(function() {
     removeClasses();
     
-    $( "#postbtn" ).addClass("btn btn-secondary");
-    $( "#questionbtn" ).addClass("btn btn-secondary");
-    $( "#reviewbtn" ).addClass("btn btn-warning");
+    $("#targetusers").removeAttr("disabled"); 
+    
+    $("#posttxt").attr("placeholder", "request a review here...");
+    
+    $( "#postbtn" ).addClass("btn btn-secondary btn-sm");
+    $( "#questionbtn" ).addClass("btn btn-secondary btn-sm");
+    $( "#reviewbtn" ).addClass("btn btn-warning btn-sm");
     
     $( "#postoptions" ).empty();
     
     $( "#postoptions" ).append(
       `
-        <div class="checkbox">
-          <label><input id="chkformal" type="checkbox" value="" checked>Informal review</label>
+        <div class="col-md-12">
+          <input id="chkformal" type="checkbox" value="" checked> Informal review</input>
         </div>
       `
     );
